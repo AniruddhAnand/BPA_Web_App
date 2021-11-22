d9a563d09c37c641b4783255966b262cf6306169
AniruddhAnand <65101767+AniruddhAnand@users.noreply.github.com>
2021-11-21 21:01:03 -0600

    Text

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:marquee/marquee.dart';
 
 class HomeScreen extends StatelessWidget {
   HomeScreen();
@@ -6,11 +7,32 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: const Text("Website"),
-      actions: <Widget>[
-        TextButton(onPressed: () {}, child: const Text("Text"))
-      ],
-    ));
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: const Text("Website"),
+        actions: <Widget>[
+          TextButton(
+              onPressed: () {},
+              child: const Text(
+                "Text",
+                style: TextStyle(color: Colors.white),
+              ))
+        ],
+      ),
+      body: Marquee(
+        text: 'Some sample text that takes some space.',
+        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.blue),
+        scrollAxis: Axis.horizontal,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        blankSpace: 20.0,
+        velocity: 100.0,
+        pauseAfterRound: Duration(seconds: 1),
+        startPadding: 10.0,
+        accelerationDuration: Duration(seconds: 1),
+        accelerationCurve: Curves.linear,
+        decelerationDuration: Duration(milliseconds: 500),
+        decelerationCurve: Curves.easeOut,
+      ),
+    );
   }
 }
