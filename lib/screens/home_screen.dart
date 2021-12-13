4d83377a4896f9e423fedda3a25039a1b93fed2c
AniruddhAnand <65101767+AniruddhAnand@users.noreply.github.com>
2021-12-12 21:57:03 -0600

    Home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,167 +10,270 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 222, 199, 181),
-      body: SingleChildScrollView(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Image.asset("assets/images/Title_Image.jpeg"),
-            Text(Data.mission,
-                style: Styles.titleTextStyleWithSecondaryTextColor),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.white,
-                    width: 5,
-                  ),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Center(
-                  child: RichText(
-                      //softWrap: true,
-                      text: TextSpan(children: <TextSpan>[
-                    TextSpan(
-                        text: Data.textMission, style: Styles.mediumTextStyle),
-                  ])),
-                ),
+      //backgroundColor: Color.fromARGB(255, 222, 199, 181),
+      body: Container(
+        constraints: const BoxConstraints.expand(),
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage("assets/images/Background.jpeg"),
+                fit: BoxFit.cover)),
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Image.asset("assets/images/Title_Image_Only2.png"),
+              Text("Mission", style: Data.style),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Data.textMission,
               ),
-            ),
-            Text(Data.sub1, style: Styles.titleTextStyleWithSecondaryTextColor),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.white,
-                    width: 5,
-                  ),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Center(
-                  child: RichText(
-                      //softWrap: true,
-                      text: TextSpan(children: <TextSpan>[
-                    TextSpan(text: Data.info1, style: Styles.mediumTextStyle),
-                  ])),
-                ),
+              Text(Data.mission, style: Data.style),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Data.info1,
               ),
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              child: Column(
-                children: <Widget>[
-                  Image.asset("assets/images/Pant.png"),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white,
-                          width: 5,
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Image.asset(
+                          "assets/images/Pant.PNG",
+                          scale: 1,
                         ),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Center(
-                        child: RichText(
-                            //softWrap: true,
-                            text: TextSpan(children: <TextSpan>[
-                          TextSpan(
-                              text: Data.pantInfo,
-                              style: Styles.mediumTextStyle),
-                        ])),
-                      ),
+                        Text("Jeans", style: Data.style),
+                      ],
                     ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              child: Column(
-                children: <Widget>[
-                  Image.asset("assets/images/Shirt.png"),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white,
-                          width: 5,
-                        ),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Center(
-                        child: RichText(
-                            //softWrap: true,
-                            text: TextSpan(children: <TextSpan>[
-                          TextSpan(
-                              text: Data.shirtInfo,
-                              style: Styles.mediumTextStyle),
-                        ])),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Container(
-              alignment: Alignment.centerLeft,
-              child: Column(
-                children: <Widget>[
-                  Image.asset("assets/images/Mushroom.png"),
-                  Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: Colors.white,
-                          width: 5,
-                        ),
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      child: Center(
-                        child: RichText(
-                            //softWrap: true,
-                            text: TextSpan(children: <TextSpan>[
-                          TextSpan(
-                              text: Data.mushInfo,
-                              style: Styles.mediumTextStyle),
-                        ])),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Text(Data.citation,
-                style: Styles.titleTextStyleWithSecondaryTextColor),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Container(
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.white,
-                    width: 5,
-                  ),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Center(
-                  child: RichText(
-                      //softWrap: true,
-                      text: TextSpan(children: <TextSpan>[
-                    TextSpan(
-                        text: Data.citationWords,
-                        style: Styles.mediumTextStyle),
-                  ])),
+                    Container(
+                        width: MediaQuery.of(context).size.width / 4,
+                        child: Data.pantInfo),
+                  ],
                 ),
               ),
-            ),
-          ],
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: <Widget>[
+                    Container(
+                        width: MediaQuery.of(context).size.width / 4,
+                        child: Data.shirtInfo),
+                    Column(
+                      children: <Widget>[
+                        Image.asset(
+                          "assets/images/Shirt_Less.png",
+                          scale: 1,
+                        ),
+                        Text("Shirt", style: Data.style),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Image.asset(
+                          "assets/images/Mushroom_Less.png",
+                          scale: 1,
+                        ),
+                        Text("Factories", style: Data.style),
+                      ],
+                    ),
+                    Container(
+                        width: MediaQuery.of(context).size.width / 4,
+                        child: Data.mushInfo),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+// import 'package:web_app/screens/image_screen.dart';
+// import 'package:web_app/values/data.dart';
+// import 'package:web_app/values/values.dart';
+// import 'package:web_app/widgets/drop_down_button.dart';
+
+// class HomeScreen extends StatelessWidget {
+//   HomeScreen();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Color.fromARGB(255, 222, 199, 181),
+//       body: SingleChildScrollView(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Image.asset("assets/images/Title_Image.jpeg"),
+//             Text(Data.mission,
+//                 style: Styles.titleTextStyleWithSecondaryTextColor),
+//             Padding(
+//               padding: const EdgeInsets.all(20.0),
+//               child: Container(
+//                 decoration: BoxDecoration(
+//                   border: Border.all(
+//                     color: Colors.white,
+//                     width: 5,
+//                   ),
+//                   borderRadius: BorderRadius.circular(10.0),
+//                 ),
+//                 child: Center(
+//                   child: RichText(
+//                       //softWrap: true,
+//                       text: TextSpan(children: <TextSpan>[
+//                     TextSpan(
+//                         text: Data.textMission, style: Styles.mediumTextStyle),
+//                   ])),
+//                 ),
+//               ),
+//             ),
+//             Text(Data.sub1, style: Styles.titleTextStyleWithSecondaryTextColor),
+//             Padding(
+//               padding: const EdgeInsets.all(20.0),
+//               child: Container(
+//                 decoration: BoxDecoration(
+//                   border: Border.all(
+//                     color: Colors.white,
+//                     width: 5,
+//                   ),
+//                   borderRadius: BorderRadius.circular(10.0),
+//                 ),
+//                 child: Center(
+//                   child: RichText(
+//                       //softWrap: true,
+//                       text: TextSpan(children: <TextSpan>[
+//                     TextSpan(text: Data.info1, style: Styles.mediumTextStyle),
+//                   ])),
+//                 ),
+//               ),
+//             ),
+//             Container(
+//               alignment: Alignment.centerLeft,
+//               child: Column(
+//                 children: <Widget>[
+//                   Image.asset("assets/images/Pant.png"),
+//                   Padding(
+//                     padding: const EdgeInsets.all(20.0),
+//                     child: Container(
+//                       decoration: BoxDecoration(
+//                         border: Border.all(
+//                           color: Colors.white,
+//                           width: 5,
+//                         ),
+//                         borderRadius: BorderRadius.circular(10.0),
+//                       ),
+//                       child: Center(
+//                         child: RichText(
+//                             //softWrap: true,
+//                             text: TextSpan(children: <TextSpan>[
+//                           TextSpan(
+//                               text: Data.pantInfo,
+//                               style: Styles.mediumTextStyle),
+//                         ])),
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             Container(
+//               alignment: Alignment.centerLeft,
+//               child: Column(
+//                 children: <Widget>[
+//                   Image.asset("assets/images/Shirt.png"),
+//                   Padding(
+//                     padding: const EdgeInsets.all(20.0),
+//                     child: Container(
+//                       decoration: BoxDecoration(
+//                         border: Border.all(
+//                           color: Colors.white,
+//                           width: 5,
+//                         ),
+//                         borderRadius: BorderRadius.circular(10.0),
+//                       ),
+//                       child: Center(
+//                         child: RichText(
+//                             //softWrap: true,
+//                             text: TextSpan(children: <TextSpan>[
+//                           TextSpan(
+//                               text: Data.shirtInfo,
+//                               style: Styles.mediumTextStyle),
+//                         ])),
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             Container(
+//               alignment: Alignment.centerLeft,
+//               child: Column(
+//                 children: <Widget>[
+//                   Image.asset("assets/images/Mushroom.png"),
+//                   Padding(
+//                     padding: const EdgeInsets.all(20.0),
+//                     child: Container(
+//                       decoration: BoxDecoration(
+//                         border: Border.all(
+//                           color: Colors.white,
+//                           width: 5,
+//                         ),
+//                         borderRadius: BorderRadius.circular(10.0),
+//                       ),
+//                       child: Center(
+//                         child: RichText(
+//                             //softWrap: true,
+//                             text: TextSpan(children: <TextSpan>[
+//                           TextSpan(
+//                               text: Data.mushInfo,
+//                               style: Styles.mediumTextStyle),
+//                         ])),
+//                       ),
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             Text(Data.citation,
+//                 style: Styles.titleTextStyleWithSecondaryTextColor),
+//             Padding(
+//               padding: const EdgeInsets.all(20.0),
+//               child: Container(
+//                 decoration: BoxDecoration(
+//                   border: Border.all(
+//                     color: Colors.white,
+//                     width: 5,
+//                   ),
+//                   borderRadius: BorderRadius.circular(10.0),
+//                 ),
+//                 child: Center(
+//                   child: RichText(
+//                       //softWrap: true,
+//                       text: TextSpan(children: <TextSpan>[
+//                     TextSpan(
+//                         text: Data.citationWords,
+//                         style: Styles.mediumTextStyle),
+//                   ])),
+//                 ),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
